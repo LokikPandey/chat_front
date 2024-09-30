343a0840da8ff80e1db291d07c7f1ad9c0e5cd39
LokikPandey <91693561+LokikPandey@users.noreply.github.com>
2024-10-01 04:45:24 +0530

    Update routes.js

--- src/assets/routes.js
+++ src/assets/routes.js
@@ -1,7 +1,7 @@
-export const host = 'https://chat-back-d48r.vercel.app/';
-export const registerroute=`${host}/api/auth/register`
-export const loginroute=`${host}/api/auth/login`
-export const avatarroute=`${host}/api/auth/avatar`
-export const alluserroute = `${host}/api/auth/allusers`
-export const addmessage=`${host}/api/messages/addmessage`
-export const getmessage=`${host}/api/messages/getmessage`
+export const host = 'https://chat-back-d48r.vercel.app';
+export const registerroute = `${host}/api/auth/register`;
+export const loginroute = `${host}/api/auth/login`;
+export const avatarroute = `${host}/api/auth/avatar`;
+export const alluserroute = `${host}/api/auth/allusers`;
+export const addmessage = `${host}/api/messages/addmessage`;
+export const getmessage = `${host}/api/messages/getmessage`;
