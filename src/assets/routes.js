5a6030b0d1d72b3b16db14e1f3131c33e386fdc9
LokikPandey <91693561+LokikPandey@users.noreply.github.com>
2024-10-12 21:49:48 +0530

    Update routes.js

--- src/assets/routes.js
+++ src/assets/routes.js
@@ -1,4 +1,4 @@
-export const host = 'https://chat-back-d48r.vercel.app';
+export const host = 'https://chat-back-vxiq.onrender.com';
 export const registerroute = `${host}/api/auth/register`;
 export const loginroute = `${host}/api/auth/login`;
 export const avatarroute = `${host}/api/auth/avatar`;
